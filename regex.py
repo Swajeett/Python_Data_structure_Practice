7289dd8b3b989c131a4b6fe894466090ae4122d8
Swajeett <swajitbhagwatsoftwareengineer@gmail.com>
2025-12-12 12:21:16 +0530

    Add files via upload

---
+++ regex.py
@@ -0,0 +1,15 @@
+# A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+
+# RegEx can be used to check if a string contains the specified search pattern.
+
+import re
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$",txt)
+# if x :
+#     print("Yes! We have a match!")
+# else:
+#     print("No match")
+
+txt = "The rain in Spain"
+x= re.findall("ai", txt)
+print(x)
